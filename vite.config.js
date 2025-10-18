e9ada7ba40a076eeaf2c0f7740f7592dc953d1d7
Paolo Marchetti <paolo.marchetti.it@gmail.com>
2025-10-18 15:30:01 +0200

    build: migrate project from Create React App to Vite

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'                                                                                                                                                              
+import react from '@vitejs/plugin-react'                                                                                                                                                         
+import { lingui } from '@lingui/vite-plugin'                                                                                                                                                     
+                                                                                                                                                                                                 
+export default defineConfig({                                                                                                                                                                    
+  plugins: [                                                                                                                                                                                     
+    react({                                                                                                                                                                                      
+      babel: {                                                                                                                                                                                   
+        plugins: ['macros']                                                                                                                                                                      
+      }                                                                                                                                                                                          
+    }),                                                                                                                                                                                          
+    lingui()                                                                                                                                                                                     
+  ],                                                                                                                                                                                             
+  server: {                                                                                                                                                                                      
+    port: 3000                                                                                                                                                                                   
+  },                                                                                                                                                                                             
+  css: {                                                                                                                                                                                         
+    preprocessorOptions: {                                                                                                                                                                       
+      scss: {                                                                                                                                                                                    
+        includePaths: ['node_modules']                                                                                                                                                           
+      }                                                                                                                                                                                          
+    }                                                                                                                                                                                            
+  }                                                                                                                                                                                              
+})
